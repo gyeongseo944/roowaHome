53a8b7c9b7ceb7f9e75fa5891bfa499de28b87cd
gyeongseo944 <85339587+gyeongseo944@users.noreply.github.com>
2022-08-11 18:15:37 +0900

    connect to notion api

--- client/src/components/Article/article.js
+++ client/src/components/Article/article.js
@@ -2,22 +2,14 @@ import "./Article.scss";
 import React, { useEffect, useRef, useState } from "react";
 import axios from 'axios'
 
+
 const Article = () => {
+  const [Article, setArticle] = useState(Object);
   useEffect(()=>{
-    // fetch("https://api.notion.com/v1/databases/45ea2c4b7c4d4a2fa4b2c3f90992cd5e/",{
-    //   headers:{
-    //     Authorization:"Bearer secret_RyfU3Nl2dInDmfmVE2tkjlVLyf2Br82wsqoJUZKGLJN",
-    //     "Notion-Version":"2022-02-22"
-    //   }
-    // })
-    //   .then((res) => res.json())
-    //   .then((res) => {
-    //     console.log(res)
-    //   });
-    fetch("/api/test")
-      .then((res) => {
-        console.log(res)
-      });
+    axios.get('/api/test').then((res)=>{
+      console.log(res)
+      setArticle(res)
+    })
   },[])
 
 
@@ -25,7 +17,6 @@ const Article = () => {
     <>
       <div className="container contact_page">
         <div style={{height:"1200px"}}>
-          <div style={{width:"200px",height:"18vh",background:"red"}}></div>
           <div style={{color:'black'}}>ggggg</div>
         </div>
       </div>
--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const models = require('./models/index');
 const cors = require('cors');
+const {Client} = require('@notionhq/client');
+const notion = new Client({auth:process.env.NOTION_TOKEN});
+
 
 app.use(cors())
 
@@ -11,13 +15,22 @@ models.sequelize.sync().then(()=>{
     console.log('DB 연결 에러')
     console.log(err)
 })
-
-app.get('/',(req,res)=>{
-    res.send('hello');
-})
-app.get('/test',(req,res)=>{
-    console.log("dfadsfdafasd")
-    res.send('hello');
+const test = async(e)=>{
+    let test1 = new Object;
+    test1.title = await notion.pages.properties.retrieve({page_id:e,property_id:'title'});
+    test1.img = await notion.pages.properties.retrieve({page_id:e,property_id:'UDr%3F'});
+    test1.date = await notion.pages.properties.retrieve({page_id:e,property_id:'JiHn'});
+    return test1
+}
+app.get('/test',async(req,res)=>{
+    let idArr = await notion.databases.query({
+        database_id:process.env.NOTION_DB_ID
+    }).then((result)=>{
+       return result.results.map(id => id.id)
+    })
+    const result = await Promise.all(idArr.map(id=>test(id)))
+    res.send(result)
+    
 })
 
 app.listen(6000);
