e4a23c29b2aadf2634a3d3b3d05062f1a898cf85
gyeongseo944 <85339587+gyeongseo944@users.noreply.github.com>
2022-08-12 11:59:16 +0900

    install antd

--- client/src/components/Article/article.js
+++ client/src/components/Article/article.js
@@ -1,6 +1,8 @@
 import "./Article.scss";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Row } from "antd";
+import card from "./card/card";
 
 const Article = () => {
   const [Article, setArticle] = useState([]);
@@ -13,10 +15,13 @@ const Article = () => {
 
   return (
     <>
-      <div className="container contact_page">
-        <div style={{ height: "1200px" }}>
-          <div style={{ color: "black" }}>ggggg</div>
-        </div>
+      <div className="article_container">
+        <Row gutter={[32, 32]}>
+          {Article &&
+            Article.map((article, index) => {
+              <React.Fragment key={index}></React.Fragment>;
+            })}
+        </Row>
       </div>
     </>
   );
