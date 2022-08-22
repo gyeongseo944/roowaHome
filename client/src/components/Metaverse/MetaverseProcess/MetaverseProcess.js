7d2097973be4de01b946bfe5c19bb14c12d8ea42
TaeHo <mun05170@gmail.com>
2022-08-22 18:31:59 +0900

    Metaverse text animate

--- client/src/components/Metaverse/MetaverseProcess/MetaverseProcess.js
+++ client/src/components/Metaverse/MetaverseProcess/MetaverseProcess.js
@@ -1,10 +1,54 @@
+import { useInView, motion, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
 import "./MetaverseProcess.scss";
 
 function MetaverseProcess() {
+  const ref = useRef();
+  const inView = useInView(ref, { amount: 0.4 });
+  const iconAnimate = useAnimation();
+  useEffect(() => {
+    iconAnimate.start({
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1,
+      },
+    });
+  }, [inView]);
+
+  // const parentsVariants = {
+  //   start: {
+  //     opacity: 0,
+  //   },
+  //   end: {
+  //     opacity: 1,
+  //     transition: {
+  //       delay: 2,
+  //       duration: 1.5,
+  //       delayChildren: 1.5,
+  //       staggerChildren: 0.5,
+  //     },
+  //   },
+  // };
+
+  const variants = {
+    start: {
+      opacity: 0,
+      y: 20,
+    },
+    end: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 1,
+      },
+    },
+  };
+
   return (
     <section className="metaverseProcess">
       <div className="title">process</div>
-      <div className="iconArea">
+      <div className="iconArea" ref={ref}>
         <div className="dashBox"></div>
         <div className="iconCircles">
           <img
@@ -31,24 +75,44 @@ function MetaverseProcess() {
           />
         </div>
       </div>
-      <div className="stepLine">
-        <div className="stepBox">
+      <motion.div
+        className="stepLine"
+        initial={{ opacity: 1 }}
+        animate={iconAnimate}
+      >
+        <motion.div
+          className="stepBox"
+          initial={{ opacity: 0, y: 20 }}
+          animate={iconAnimate}
+        >
           <div className="step">step 01</div>
           <div className="title">고객사 상담 미팅</div>
-        </div>
-        <div className="stepBox">
+        </motion.div>
+        <motion.div
+          className="stepBox"
+          initial={{ opacity: 0, y: 20 }}
+          animate={iconAnimate}
+        >
           <div className="step">step 02</div>
           <div className="title">월드맵 기획 작업</div>
-        </div>
-        <div className="stepBox">
+        </motion.div>
+        <motion.div
+          className="stepBox"
+          initial={{ opacity: 0, y: 20 }}
+          animate={iconAnimate}
+        >
           <div className="step">step 03</div>
           <div className="title">{"모델링 및 앱 상호작용 개발"}</div>
-        </div>
-        <div className="stepBox">
+        </motion.div>
+        <motion.div
+          className="stepBox"
+          initial={{ opacity: 0, y: 20 }}
+          animate={iconAnimate}
+        >
           <div className="step">step 04</div>
           <div className="title">제작 완성</div>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </section>
   );
 }
--- client/src/components/Metaverse/MetaverseYouTube/MetaverseYouTube.js
+++ client/src/components/Metaverse/MetaverseYouTube/MetaverseYouTube.js
@@ -21,7 +21,7 @@ function MetaverseYouTube() {
         <span>
           {"루와의 메타버스 마케팅은\n가장 클라이언트의 목적에 부합하는\n"}
           <strong>메타버스 컨셉</strong>과{" "}
-          <string>월드맵 기획, 제작, 브렌디드 굿즈</string>
+          <strong>월드맵 기획, 제작, 브렌디드 굿즈</strong>
           {"를 포함한\n모든 활용과 "}
           <strong>홍보 서비스</strong>를 제공합니다
         </span>
