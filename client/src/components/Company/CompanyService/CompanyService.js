404b1a526b029a9f23e023cd7bf6989c13e782f5
gyeongseo944 <85339587+gyeongseo944@users.noreply.github.com>
2022-08-31 10:09:53 +0900

    roowa team component

--- client/src/components/Company/CompanyService/CompanyService.js
+++ client/src/components/Company/CompanyService/CompanyService.js
@@ -2,7 +2,19 @@ import React from "react";
 import "./CompanyService.scss";
 
 const CompanyService = () => {
-  return <div>CompanyService</div>;
+  return (
+    <div className="serviceContainer">
+      <div className="serviceTitle">
+        <h1>루와의 서비스</h1>
+        <p>
+          루와는 <span>콘텐츠 마케팅 서비스</span>를 통해 고객사의 목표를 이루어드리는
+          <br />
+          고객사의 목표를, 꿈을, 이루어드리는 기업입니다.
+        </p>
+      </div>
+      <img src={team} alt="roowa team" />
+    </div>
+  );
 };
 
 export default CompanyService;
--- client/src/components/Company/CompanyTeam/CompanyTeam.js
+++ client/src/components/Company/CompanyTeam/CompanyTeam.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./CompanyTeam.scss";
+import team from "../../../assets/company/team.png";
 
 const CompanyTeam = () => {
-  return <div>CompanyTeam</div>;
+  return (
+    <div className="companyTeamContainer">
+      <div className="teamTitle">
+        <h1>팀, 루와</h1>
+        <p>
+          루와는 <span>'마음을 움직이는 콘텐츠'</span>를 만든다는 슬로건 아래
+          <br />
+          고객사의 목표를, 꿈을, 이루어드리는 기업입니다.
+        </p>
+      </div>
+      <img src={team} alt="roowa team" />
+    </div>
+  );
 };
 
 export default CompanyTeam;
